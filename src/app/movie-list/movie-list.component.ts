4978f702970d0f13541888258c8fcf493a6f7a7a
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-19 15:53:51 -0400

    added api callback and check within browser

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { MovieService } from "../Services/movie.service";
 @Component({
-  selector: 'app-movie-list',
-  templateUrl: './movie-list.component.html',
-  styleUrls: ['./movie-list.component.css']
+  selector: "app-movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"]
 })
 export class MovieListComponent implements OnInit {
+  movieData: any;
+  constructor(private movieService: MovieService) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  getMovieData(): any {
+    console.log("Get Movie Data works (Movie-list component)");
+    this.movieService.getMovieData().then(response => {
+      this.movieData = response.data;
+    });
   }
 
+  ngOnInit() {
+    console.log("Get Movie Data works (Movie-list component)");
+    this.movieService.getMovieData().then(response => {
+      this.movieData = response.data;
+    });
+  }
 }
