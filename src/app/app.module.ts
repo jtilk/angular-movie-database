dbaa6b470cf7931aa7e6ee39f1ad5c6aedce9140
kody <kodypeters91@gmail.com>
2019-08-19 13:59:10 -0400

    Added component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    MovieListComponent,
+    WatchlistPageComponent
   ],
   imports: [
     BrowserModule
