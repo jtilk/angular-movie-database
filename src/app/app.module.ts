45691fef6574db4f70afdb5d8c803776e9baf10c
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-19 14:33:27 -0400

    added services folder and combined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
-import { MovieListComponent } from './movie-list/movie-list.component';
-import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MovieService } from "./Services/movie.service";
+import { RoutingService } from "./Services/routing.service";
+import { AppComponent } from "./app.component";
+import { SearchCriteriaComponent } from "./search-criteria/search-criteria.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { WatchlistPageComponent } from "./watchlist-page/watchlist-page.component";
 
 @NgModule({
   declarations: [
@@ -13,10 +14,8 @@ import { WatchlistPageComponent } from './watchlist-page/watchlist-page.componen
     MovieListComponent,
     WatchlistPageComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, MovieService, RoutingService],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
