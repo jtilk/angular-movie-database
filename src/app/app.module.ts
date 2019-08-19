07b785dec05e349a34549bdb133362d11005fb8c
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-19 16:29:47 -0400

    combined components into movie-master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from "@angular/core";
 import { MovieService } from "./Services/movie.service";
 import { RoutingService } from "./Services/routing.service";
 import { AppComponent } from "./app.component";
-import { SearchCriteriaComponent } from "./search-criteria/search-criteria.component";
-import { MovieListComponent } from "./movie-list/movie-list.component";
-import { WatchlistPageComponent } from "./watchlist-page/watchlist-page.component";
+import { SearchCriteriaComponent } from "./movie-master/search-criteria/search-criteria.component";
+import { MovieListComponent } from "./movie-master/movie-list/movie-list.component";
+import { WatchlistPageComponent } from "./movie-master/watchlist-page/watchlist-page.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 
@@ -17,7 +17,7 @@ import { FormsModule } from "@angular/forms";
     WatchlistPageComponent
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [MovieService],
+  providers: [MovieService, RoutingService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
