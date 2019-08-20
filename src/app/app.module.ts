01afef3dff4ca6183f5642399f494fdfc57bb159
kody <kodypeters91@gmail.com>
2019-08-20 10:29:15 -0400

    restructured project directories and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from "@angular/core";
 import { MovieService } from "./Services/movie.service";
 import { RoutingService } from "./Services/routing.service";
 import { AppComponent } from "./app.component";
-import { SearchCriteriaComponent } from "./movie-master/search-criteria/search-criteria.component";
-import { MovieListComponent } from "./movie-master/movie-list/movie-list.component";
-import { WatchlistPageComponent } from "./movie-master/watchlist-page/watchlist-page.component";
+import { SearchCriteriaComponent } from "./search-criteria/search-criteria.component";
+import { MovieListComponent } from "./movie-list/movie-list.component";
+import { WatchlistPageComponent } from "./watchlist-page/watchlist-page.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-import { MovieMasterComponent } from './movie-master/movie-master.component';
+import { MovieCardComponent } from './movie-list/movie-card/movie-card.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TopnavComponent } from './topnav/topnav.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { MovieMasterComponent } from './movie-master/movie-master.component';
     SearchCriteriaComponent,
     MovieListComponent,
     WatchlistPageComponent,
-    MovieMasterComponent
+    MovieCardComponent,
+    SidenavComponent,
+    TopnavComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [MovieService, RoutingService],
