999580ce042f344d6653071a93d9dd1b4cb40824
Brendan Cowley <b.cowley1220@gmail.com>
2019-08-21 13:10:36 -0400

    saving before the big pull

--- src/app/Services/movie.service.ts
+++ src/app/Services/movie.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { match } from "minimatch";
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Injectable({
   providedIn: "root"
 })
 export class MovieService {
-  resultsList: object[];
-  watchList: object[];
+  resultsList: object[] = [];
+  watchList: object[] = [];
+  genreList: object[] = [];
+  filterList: object[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -41,11 +45,12 @@ export class MovieService {
       )
       .toPromise();
   }
-  //   getPopMovieData(searchQuery: string): any {
-  //     return this.http
-  //       .get(
-  //         `https://api.themoviedb.org/3/movie/popular?api_key=e5ae3d31b67ec5459b5a39058c5630cd&language=en-US&page=1`.toPromise();
 
-  //       )}
-  // }
+  filterMovies(form: NgForm): any {
+    console.log(form);
+    this.filterList = this.resultsList.filter(
+      movie => movie["genre_ids"][0] === form.options["name"]
+    );
+    console.log(this.filterList);
+  }
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { MovieService } from "../Services/movie.service";
+import { format } from "util";
+import { FormsModule, NgForm } from "@angular/forms";
 @Component({
   selector: "app-movie-list",
   templateUrl: "./movie-list.component.html",
@@ -7,7 +9,10 @@ import { MovieService } from "../Services/movie.service";
 })
 export class MovieListComponent implements OnInit {
   movieData: object[];
-  movieGenre: any;
+  movieGenre: object[];
+  resultsList: object[];
+  filteredMovies: object[];
+  genreName: string;
 
   constructor(private movieService: MovieService) {}
 
@@ -34,6 +39,31 @@ export class MovieListComponent implements OnInit {
       console.log(this.movieData);
     });
   }
+  filterMovies(genreForm: NgForm) {
+    // for (let movie of this.movieData) {
+    //   console.log(movie);
+    //   for (let genre of this.movieGenre) {
+    //     console.log(genre);
+    //     if (movie.genre_ids[0] === genre.id) {
+    //       console.log(movie.genre.ids[0]);
+    //     }
+    //   }
+    this.movieService.filterMovies(genreForm);
+
+    // console.log("Filter movie is working");
+    // console.log(this.filteredMovies);
+    // return this.filteredMovies;
+  }
+
+  // filterMovies(): any {
+  //   // !This function needs to iterate through the current array and compare the first id# to the genre array
+  //   // !If it finds a match, it pushes that item into a new array to display
+  //   for (let movie of this.resultsList) {
+  //     if (movie.genre_ids[0] === this.genreList.id.value) {
+  //       this.filterList.push(movie);
+  //     }
+  //   }
+  // }
 
   ngOnInit() {
     console.log("Get Movie Data works (Movie-list component on init)");
